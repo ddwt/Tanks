ced427adf1c22aedc3eac15848eedfb75bfbb54e
ddwt <33974770+ddwt@users.noreply.github.com>
2018-08-18 23:57:52 +0800

    5
    玩家位置信息类型

---
+++ PhotonServer/Common/Vector3Data.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Common {
+    [Serializable]
+    public class Vector3Data {
+        public float x { get; set; }
+        public float y { get; set; }
+        public float z { get; set; }
+    }
+}
