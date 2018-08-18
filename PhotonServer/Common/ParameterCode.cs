db139035083c16b48808aca2c105ba45edc37708
ddwt <33974770+ddwt@users.noreply.github.com>
2018-08-18 23:56:39 +0800

    3
    传输数据类型

---
+++ PhotonServer/Common/ParameterCode.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Common {
+    public enum ParameterCode:byte {
+        Id,
+        Username,
+        Account,
+        Pwd,
+        Level,
+        Score,
+        x, y, z,
+        UsernameList,
+        PlayerDataList
+    }
+}
