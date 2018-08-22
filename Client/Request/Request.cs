30014abec525be8f936f6734ea14782f861e2fee
ddwt <33974770+ddwt@users.noreply.github.com>
2018-08-22 23:52:21 +0800

    Add files via upload

---
+++ Client/Request/Request.cs
@@ -0,0 +1,18 @@
+﻿using Common;
+using UnityEngine;
+using ExitGames.Client.Photon;
+
+
+public abstract class Request : MonoBehaviour {
+    public Common.OperationCode operationCode;
+    public abstract void DefaultRequest();
+    public abstract void OnOperationResponse(OperationResponse opResponse);
+
+    public virtual void Start() {
+        PhotoEngine._instance.AddRequest(this);
+    }
+
+    public virtual void OnDestroy() {
+        PhotoEngine._instance.RemoveRequest(this);
+    }
+}
